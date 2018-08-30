[fix] double softmax at option discriminator and change distill loss

baselines/vfo/models.py
@@ -129,6 +129,6 @@ def nn_discriminator(num_options=64, hidden_sizes=[100, 100],
                 h = fc(h, 'eval', nh=num_options,
                        init_scale=np.sqrt(2))
 
-        return tf.nn.softmax(h)
+        return tf.nn.softmax(h), h
 
     return network_fn

baselines/vfo/policies.py
@@ -103,8 +103,9 @@ class PolicyWithValue(object):
             print('pvfs_next:', self.next_pvfs.get_shape().as_list())
 
             with tf.variable_scope('discriminator'):
-                self.option_discriminator = nn_discriminator(
-                    num_options=self.noptions)(tf.stop_gradient(latent))
+                no_grad_latent = tf.stop_gradient(latent)
+                self.option_discriminator, self.option_discriminator_logits = \
+                    nn_discriminator(num_options=self.noptions)(no_grad_latent)
 
     def _evaluate(self, variables, observation, **extra_feed):
         sess = self.sess or tf.get_default_session()

baselines/vfo/vfo.py
@@ -108,16 +108,14 @@ class Model(object):
                 discriminator_loss = tf.reduce_mean(
                     tf.nn.softmax_cross_entropy_with_logits_v2(
                         labels=train_model.op_z,
-                        logits=train_model.option_discriminator))
+                        logits=train_model.option_discriminator_logits))
 
             with tf.variable_scope('distillation'):
                 # NOTE: to train distillation, op_z should be feed with q(z|s)
                 print('mf_pi:', train_model.pi.get_shape().as_list())
                 print('op_pi:', train_model.option_pi.get_shape().as_list())
-                distillation_loss = tf.reduce_mean(
-                    tf.nn.softmax_cross_entropy_with_logits_v2(
-                        labels=tf.stop_gradient(train_model.pi),
-                        logits=train_model.option_pi))
+                distillation_loss = losses.mean_squared_error(
+                    tf.stop_gradient(train_model.pi), train_model.option_pi)
 
         _train_option_q = tf.train.AdamOptimizer(lr).minimize(
             loss=option_q_loss, var_list=params)